7c2e1461057878c6736819cfb79320741bb3958e
wisnugautama <ngurahwisnugautama@gmail.com>
2018-09-19 21:22:16 +0700

    add form register screen

--- facelocker/src/screens/RegisterScreen.js
+++ facelocker/src/screens/RegisterScreen.js
@@ -1,12 +1,50 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet, TouchableOpacity, TextInput, Image } from 'react-native';
 
 export default class RegisterScreen extends React.Component {
     render () {
         return (
-            <View>
-                <Text>Halo</Text>
+            <View style={{ justifyContent: 'center', alignItems: 'center', backgroundColor: 'brown', height: 620}}>
+                <Image 
+                style={{ width: 80, height: 80, marginTop: 10, marginBottom: 10}}
+                source={require('../images/lockers.png')} />
+                <Text style={{ fontWeight:'500', marginBottom: 40, color: 'white', fontSize: 30 }}>Facelocker</Text>
+                
+                <TextInput 
+                style={ Style.input }
+                placeholderTextColor="white"
+                placeholder="Username" />
+
+                <TextInput 
+                style={ Style.input }
+                placeholderTextColor="white"
+                placeholder="Email" />
+
+                <TextInput 
+                style={ Style.input }
+                placeholderTextColor="white"
+                placeholder="Phone" />
+
+                <TextInput 
+                style={ Style.input }
+                placeholderTextColor="white"
+                placeholder="Password" />
+
+                <TouchableOpacity style={{ borderWidth: 1, width: 300, height: 50, borderRadius: 3, marginTop: 40, borderColor: 'yellow', backgroundColor: 'white' }}>
+                    <Text style={{ textAlign: 'center', paddingVertical: 12 }}>Submit</Text>
+                </TouchableOpacity>
             </View>
         )
     }
 }
+
+const Style = StyleSheet.create({
+    input: {
+        borderBottomWidth: 1, 
+        borderBottomColor: 'white',
+        width: 300, 
+        height: 50,
+        marginTop: 10,
+        color: 'white'
+    }
+})
